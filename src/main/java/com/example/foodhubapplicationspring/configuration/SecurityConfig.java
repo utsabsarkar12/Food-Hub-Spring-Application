2834c94598345827ef5403b02f43d7c6e9b44226
LAPTOP-32363BRT\ACER <utsab.heart@gmail.com>
2023-11-26 21:33:50 +0600

    home page modification

--- src/main/java/com/example/foodhubapplicationspring/configuration/SecurityConfig.java
+++ src/main/java/com/example/foodhubapplicationspring/configuration/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/index").permitAll()
+                        .requestMatchers("/").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
--- src/main/java/com/example/foodhubapplicationspring/controller/LoginController.java
+++ src/main/java/com/example/foodhubapplicationspring/controller/LoginController.java
@@ -13,7 +13,7 @@ public class LoginController {
 
     @GetMapping("/")
     public String home(){
-        return"index";
+        return"home";
     }
 
     @GetMapping("/index")
